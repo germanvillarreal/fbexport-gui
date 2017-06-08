7990e2c6de2ae756ef19fd0041d0eda38d93e953
German Villarreal <vo.german@gmail.com>
2017-06-08 14:31:56 -0700

    - Ask for output file on export button click - Allow SQL file query or Table with Where

--- src/main.py
+++ src/main.py
@@ -1,60 +1,124 @@
 from tkinter import *
+from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import asksaveasfile
 
 
 class App:
-  def __init__(self, master):
+
+  RADIO_URL = 1
+  RADIO_FILE = 2
+  RADIO_TABLE = 1
+  RADIO_SQL = 2
+
+  def __init__(self, master, ttk):
+
+    master.title('fbexport GUI')
+    master.resizable(False, False)
 
     menubar = Menu(master)
     menubar.add_command(label='File')
     menubar.add_command(label='About')
     master.config(menu=menubar)
 
-    master.title('fbexport GUI')
-    master.resizable(False, False)
-
-##########
+########## DATABASE INFO
     self.frame_database = LabelFrame(master, text='Connection Info', padx=5, pady=5)
     self.frame_database.pack(padx=5, pady=5, fill='x')
 
-    Label(self.frame_database, text='Database:').grid(row=0, column=0, padx=5, pady=5)
-    Entry(self.frame_database, name='browse').grid(row=0, column=1, padx=5, pady=5)
-    Button(self.frame_database, text='Browse..', command=self.load_file).grid(row=0, column=2, padx=5, pady=5)
+    self.stream = IntVar()
+    self.stream.set(1)
 
-    Label(self.frame_database, text='Username:').grid(row=1, column=0, padx=5, pady=5)
-    Entry(self.frame_database, name='dtb').grid(row=1, column=1, columnspan=2, padx=5, pady=5, sticky='we')
+    Radiobutton(self.frame_database, value=App.RADIO_URL, variable=self.stream, text='URL:', command=self.conn_type).grid(row=0, column=0, padx=5, pady=5)
+    Entry(self.frame_database, name='url').grid(row=0, column=1, columnspan=2, padx=5, pady=5, sticky='we')
 
-    Label(self.frame_database, text='Password:').grid(row=2, column=0, padx=5, pady=5)
-    Entry(self.frame_database, name='pword', show='*').grid(row=2, column=1, columnspan=2, padx=5, pady=5, sticky='we')
+    Radiobutton(self.frame_database, value=App.RADIO_FILE, variable=self.stream, text='File:', command=self.conn_type).grid(row=1, column=0, padx=5, pady=5)
+    Entry(self.frame_database, name='browse').grid(row=1, column=1, padx=5, pady=5)
+    Button(self.frame_database, name='browse_btn', text='Browse..', command=self.load_file).grid(row=1, column=2, padx=5, pady=5)
 
-##########
-    self.frame_output = LabelFrame(master, text='Output File', padx=5, pady=5)
-    self.frame_output.pack(padx=5, pady=5, fill='x')
+    Label(self.frame_database, text='Username:').grid(row=2, column=0, padx=5, pady=5)
+    Entry(self.frame_database, name='dtb').grid(row=2, column=1, columnspan=2, padx=5, pady=5, sticky='we')
 
-    Label(self.frame_output, text='File:').grid(row=0, column=0, padx=5, pady=5)
-    Entry(self.frame_output, name='ofile').grid(row=0, column=1, padx=5, pady=5)
-    Button(self.frame_output, text='Save As..', command=self.save_file).grid(row=0, column=2, padx=5, pady=5)
-
-    Label(self.frame_output, text='Format:').grid(row=1, column=0, padx=5, pady=5)
-    self.ftype = StringVar(master)
-    self.ftype.set('CSV')
-    #(S - binary, Si - INSERTs, Sc - CSV, Sh - HTML)
-    self.menu_ftype = OptionMenu(self.frame_output, self.ftype, 'binary', 'INSERTs', 'CSV', 'HTML').grid(row=1, column=1, padx=5, pady=5)
+    Label(self.frame_database, text='Password:').grid(row=3, column=0, padx=5, pady=5)
+    Entry(self.frame_database, name='pword', show='*').grid(row=3, column=1, columnspan=2, padx=5, pady=5, sticky='we')
 
 ##########
     self.frame_data = LabelFrame(master, text='Data', padx=5, pady=5)
     self.frame_data.pack(padx=5, pady=5, fill='x')
 
-    Label(self.frame_data, text='Table:').grid(row=0, column=0, padx=5, pady=5)
-    Entry(self.frame_data, name='table').grid(row=0, column=1, padx=5, pady=5)
+    self.data = IntVar()
+    self.data.set(1)
+
+    Radiobutton(self.frame_data, value=App.RADIO_TABLE, variable=self.data, command=self.data_type, text='Table:').grid(row=0, column=0, padx=5, pady=5, columnspan=2)
+    Entry(self.frame_data, name='table').grid(row=0, column=2, columnspan=2, padx=5, pady=5, sticky='we')
+
+    Label(self.frame_data, text='Where:').grid(row=1, column=1, padx=5, pady=5, sticky='we')
+    Entry(self.frame_data, name='where').grid(row=1, column=2, columnspan=2, sticky='we', padx=5, pady=5)
+
+    Radiobutton(self.frame_data, value=App.RADIO_SQL, variable=self.data, command=self.data_type, text='SQL:').grid(row=2, column=0, padx=5, pady=5, columnspan=2)
+    Entry(self.frame_data, name='sql').grid(row=2, column=2, padx=5, pady=5)
+    Button(self.frame_data, name='sql_btn', text='Browse..', command=self.load_file).grid(row=2, column=3, padx=5, pady=5)
 
 ##########
-    self.btn_export = Button(master, text='Export', command=self.export)
-    self.btn_export.pack(padx=5, pady=5)
+    self.action_frame = Frame(master)
+    self.action_frame.pack()
+    Button(self.action_frame, text='Save As...').grid(row=0, column=0, padx=5, pady=5)
+    Button(self.action_frame, text='Export As...', command=self.export()).grid(row=0, column=1, padx=5, pady=5)
+
+    self.data_type()
+    self.conn_type()
+
+
+  def save(self):
+    return
+
+  def data_type(self):
+    radio_val = self.data.get()
+    data_children = self.frame_data.children
+    table_widgets = [data_children['table'], data_children['where']]
+    sql_widgets = [data_children['sql'], data_children['sql_btn']]
+
+    if radio_val == App.RADIO_TABLE:
+      for widget in sql_widgets:
+        widget.configure(state='disable')
+
+      for widget in table_widgets:
+        widget.configure(state='normal')
+
+      table_widgets[0].focus()
+    elif radio_val == App.RADIO_SQL:
+      for widget in table_widgets:
+        widget.configure(state='disable')
+
+      for widget in sql_widgets:
+        widget.configure(state='normal')
+
+      sql_widgets[0].focus()
+
+  def conn_type(self):
+    radio_val = self.stream.get()
+    dtb_children = self.frame_database.children
+    browse_widgets = [dtb_children['browse'], dtb_children['browse_btn']]
+    url_widgets = [dtb_children['url']]
+
+    if radio_val == App.RADIO_URL:
+      for widget in browse_widgets:
+        widget.configure(state='disable')
+
+      for widget in url_widgets:
+        widget.configure(state='normal')
+      url_widgets[0].focus()
+
+    elif radio_val == App.RADIO_FILE:
+      for widget in url_widgets:
+        widget.configure(state='disable')
+
+      for widget in browse_widgets:
+        widget.configure(state='normal')
+      browse_widgets[0].focus()
+
 
   def load_file(self):
-    name = askopenfilename(filetypes=[("Firebird Database","*.fdb")])
+    name = askopenfilename(filetypes=[("Firebird Database", "*.fdb")])
 
     self.frame_database.children['browse'].insert(0, name)
     self.frame_database.children['browse'].xview(END)
@@ -84,5 +148,5 @@ class App:
     return
 
 root = Tk()
-app = App(root)
+app = App(root, ttk)
 root.mainloop()
